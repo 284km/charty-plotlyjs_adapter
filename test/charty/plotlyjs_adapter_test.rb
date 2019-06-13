2ec1250b9c5c4d1a03f9085adb828c860424d766
284km <k.furuhashi10@gmail.com>
2019-06-13 21:35:53 +0900

    Use test-unit

--- test/charty/plotlyjs_adapter_test.rb
+++ test/charty/plotlyjs_adapter_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Charty::PlotlyjsAdapterTest < Minitest::Test
+class Charty::PlotlyjsAdapterTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::Charty::PlotlyjsAdapter::VERSION
   end
